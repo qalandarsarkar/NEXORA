ed7aa0900bbe54d1fd0c3fd8744a12ae3dd28382
qalandarsarkar <qalandarsarkar42@gmail.com>
2026-05-26 02:13:38 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// NEXORA PRO - Locked Firebase Configuration Credentials
+export const firebaseConfig = {
+  apiKey: "AIzaSyDK1uftei96IVay2mOlgXZMbmQlFWMV3GY",
+  authDomain: "nexora-81b15.firebaseapp.com",
+  projectId: "nexora-81b15",
+  storageBucket: "nexora-81b15.firebasestorage.app",
+  messagingSenderId: "17753817995",
+  appId: "1:17753817995:web:a2c91f139402d2166d16ee"
+};
